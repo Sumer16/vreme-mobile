08b9e5a4d6b1f80444ae65b7ffe8984b31bba326
Sumer Patel <patelsum@oregonstate.edu>
2023-11-10 23:54:11 -0800

    Added mocked locations for search bar

--- screens/Home.js
+++ screens/Home.js
@@ -6,7 +6,12 @@ import { MagnifyingGlassIcon, XMarkIcon } from 'react-native-heroicons/outline';
 import { CalendarDaysIcon, MapPinIcon } from 'react-native-heroicons/solid';
 
 export default function Home() {
-  const [ showSearch, isShowSearch ] = useState(false);
+  const [ showSearch, setShowSearch ] = useState(false);
+  const [ locations, setLocations ] = useState([11, 2, 3, 5]);
+
+  const handleLocation = () => {
+    console.log('Location!');
+  }
 
   return (
     <View className="flex-1 relative">
@@ -36,7 +41,7 @@ export default function Home() {
               ) : null
             }
             <TouchableOpacity
-              onPress={() => isShowSearch(!showSearch)}
+              onPress={() => setShowSearch(!showSearch)}
               style={{ backgroundColor: theme.bgWhite(0.3)}}
               className="rounded-full p-3 m-1"
             >
@@ -45,6 +50,29 @@ export default function Home() {
               </Text>
             </TouchableOpacity>
           </View>
+          {
+            locations.length > 0 && showSearch ? 
+            (
+              <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+                {
+                  locations.map((location, index) => {
+                    let showBorder = index+1 != locations.length;
+                    let borderClass = showBorder ? "border-b-2 border-b-gray-400" : "";
+                    return (
+                      <TouchableOpacity 
+                        onPress={() => handleLocation(location)}
+                        key={index} 
+                        className={"flex flex-row p-3 border-0 px-4 mb-1 " + borderClass}
+                      >
+                        <MapPinIcon size={20} color="gray" />
+                        <Text className="text-zinc-700 text-lg ml-2">Corvallis, Oregon</Text>
+                      </TouchableOpacity>
+                    )
+                  })
+                }
+              </View>
+            ) : null
+          }
         </View>
       </SafeAreaView>
     </View>
